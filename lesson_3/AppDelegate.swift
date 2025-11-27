0f8a6a33a15519b29771c9de440bd4c0796e1bd9
Egor <egor.braginov@gmail.com>
2025-11-27 21:41:59 +0300

    add let a

--- lesson_3/AppDelegate.swift
+++ lesson_3/AppDelegate.swift
@@ -10,13 +10,14 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-let a = 123
+
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
 
+let a = 123
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
