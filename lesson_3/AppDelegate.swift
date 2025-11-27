f53fb730491ea1cc4b5eaba927dbdcded5bb9ccc
Egor <egor.braginov@gmail.com>
2025-11-27 21:28:54 +0300

    add let b

--- lesson_3/AppDelegate.swift
+++ lesson_3/AppDelegate.swift
@@ -11,6 +11,9 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 let greetings = "hello, world!"
+    
+let b = "fuck"
+
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
